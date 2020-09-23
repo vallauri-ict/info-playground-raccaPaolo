204cba42e2092c8db3d55316d05d2a1056228989
raccaPaolo <62544162+raccaPaolo@users.noreply.github.com>
2020-09-23 10:44:35 +0200

    First commit

---
+++ 01_MultiForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _01_MultiForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnForm2 = new System.Windows.Forms.Button();
+            this.btnForm2B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnForm2
+            // 
+            this.btnForm2.Location = new System.Drawing.Point(13, 13);
+            this.btnForm2.Name = "btnForm2";
+            this.btnForm2.Size = new System.Drawing.Size(75, 23);
+            this.btnForm2.TabIndex = 0;
+            this.btnForm2.Text = "Apri Form2";
+            this.btnForm2.UseVisualStyleBackColor = true;
+            this.btnForm2.Click += new System.EventHandler(this.btnForm2_Click);
+            // 
+            // btnForm2B
+            // 
+            this.btnForm2B.Location = new System.Drawing.Point(94, 13);
+            this.btnForm2B.Name = "btnForm2B";
+            this.btnForm2B.Size = new System.Drawing.Size(75, 23);
+            this.btnForm2B.TabIndex = 1;
+            this.btnForm2B.Text = "Apri Form2B";
+            this.btnForm2B.UseVisualStyleBackColor = true;
+            this.btnForm2B.Click += new System.EventHandler(this.btnForm2B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnForm2B);
+            this.Controls.Add(this.btnForm2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnForm2;
+        private System.Windows.Forms.Button btnForm2B;
+    }
+}
+
---
+++ 01_MultiForm/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_MultiForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();//new istanza l'oggetto
+            f2.Show();
+        }
+
+        private void btnForm2B_Click(object sender, EventArgs e)
+        {
+            Form f2b = new Form();//creo dinamicamente, senza aggiungere prima classe con classe di default di C#
+            f2b.Text = "Form dinamica aperta";
+            f2b.Show();
+        }
+    }
+}
