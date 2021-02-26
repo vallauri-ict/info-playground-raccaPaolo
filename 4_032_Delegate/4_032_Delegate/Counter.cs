f7e1e8a8dab0aa07e735d40eed962340c8fdecc5
raccaPaolo <p.racca.1368@vallauri.edu>
2021-02-26 10:33:38 +0100

    Continued Ex. 4_032

---
+++ 4_032_Delegate/4_032_Delegate/Counter.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_032_Delegate
+{
+    public delegate void OverMaxEventHandler(object sender, OverMaxEventArgs e);
+    class Counter
+    {
+        private int maxVal;
+        public Counter(int n)
+        {
+            if (n>10)
+            {
+                throw new Exception("Il valore massimo è 10");
+            }
+            else
+            {
+                maxVal = n;
+            }
+        }
+    }
+}
---
+++ 4_032_Delegate/4_032_Delegate/OverMaxEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace _4_032_Delegate
+{
+    public class OverMaxEventArgs
+    {
+        private int valoreSoglia;
+
+        public OverMaxEventArgs(int valoreSoglia)
+        {
+            ValoreSoglia = valoreSoglia;
+        }
+
+        public int ValoreSoglia { get => valoreSoglia; set => valoreSoglia = value; }
+    }
+}
