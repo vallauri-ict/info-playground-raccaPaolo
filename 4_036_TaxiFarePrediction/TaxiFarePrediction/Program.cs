5313a49eb43f8746e7f76324fb5e98cd637fd78f
raccaPaolo <p.racca.1368@vallauri.edu>
2021-05-28 09:41:24 +0200

    Started Ex. 4_036 using AI

---
+++ 4_036_TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace TaxiFarePrediction
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext(seed: 0);
+            var model = Train(mlContext, _trainDataPath);
+        }
+
+    }
+}
